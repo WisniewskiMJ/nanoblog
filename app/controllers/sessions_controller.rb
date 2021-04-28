e3e18c72cae04f67fddcded1388b2f09110f4325
Bakshee <bakshee.jw@gmail.com>
2021-04-28 20:38:41 +0200

    Add sessions controller actions and login view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,21 @@
 class SessionsController < ApplicationController
+  include SessionsHelper
+  
   def new
   end
 
   def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      login(user)
+      redirect_to user_url(user)
+    else
+      render :new
+    end
   end
 
   def destroy
+    logout
+    redirect_to root_url
   end
 end
