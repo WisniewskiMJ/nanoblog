a0733171b7a9da25daf7b477663185a0a94f8eab
Bakshee <bakshee.jw@gmail.com>
2021-04-28 14:03:32 +0200

    Add user model validations tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to have_secure_password }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_least(2).is_at_most(30) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email) }
+    it { is_expected.to allow_value('valid@email.address').for(:email) }
+    it { is_expected.to_not allow_value('invalid@email_address').for(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_length_of(:password).is_at_least(8) }
+    it { is_expected.to validate_presence_of(:password_confirmation) }
+  end
 end
