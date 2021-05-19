f939b55eb139890f82826aca7c2422f722f4941b
Bakshee <bakshee.jw@gmail.com>
2021-05-19 18:00:09 +0200

    Add password resets controller #create

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,6 +3,14 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:reset][:email])
+    if @user
+      flash[:info] = 'Password reset link has been sent'
+      redirect_to root_url
+    else
+      flash[:danger] = 'This email address could not be found'
+      render 'new'
+    end
   end
 
   def edit
