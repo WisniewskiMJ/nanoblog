5c34dac80b96e24ed8d150b9dfb676a2c6762f94
Bakshee <bakshee.jw@gmail.com>
2021-05-20 14:06:05 +0200

    Fix mailers tests

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,10 +1,10 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
+  let(:user) { FactoryBot.create(:user) }
+  
   describe "account_activation" do
-    let(:user) { FactoryBot.create(:user) }
     let(:mail) { UserMailer.account_activation(user) }
-
     describe "headers" do
       it "is titled 'Account activation'" do
         expect(mail.subject).to eq("Account activation")
@@ -27,18 +27,31 @@ RSpec.describe UserMailer, type: :mailer do
     end
   end
 
-  pending "password_reset" do
-    let(:mail) { UserMailer.password_reset }
-
-    it "renders the headers" do
-      expect(mail.subject).to eq("Password reset")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+  describe "password_reset" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:mail) { UserMailer.password_reset(user) }
+    before do
+      user.set_reset_digest
     end
-
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    describe "headers" do
+      it "is titled 'Password reset'" do
+        expect(mail.subject).to eq("Password reset")
+      end
+      it "is sent to user email address" do
+        expect(mail.to).to eq([user.email])
+      end
     end
-  end
-
+  
+      describe "body" do
+        it "includes user name" do
+          expect(mail.body.encoded).to match(user.name)
+        end
+        it "includes escaped user email address" do
+          expect(mail.body.encoded).to match(CGI.escape(user.email))
+        end
+        it "includes user reset token" do
+          expect(mail.body.encoded).to match(user.reset_token)
+        end
+      end
+    end
 end
