9b4da3ef3bf67a5baa3adf0847ed26c55b81428e
Bakshee <bakshee.jw@gmail.com>
2021-06-01 20:16:02 +0200

    Add post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_posts_on_user_id                 (user_id)
+#  index_posts_on_user_id_and_created_at  (user_id,created_at)
+#
+# Foreign Keys
+#
+#  user_id  (user_id => users.id)
+#
 class Post < ApplicationRecord
+  validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 150 }
+  
   belongs_to :user
 end
