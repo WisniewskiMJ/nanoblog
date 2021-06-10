365d0f43a10058e48c06e45dad42368fb2f1edca
Bakshee <bakshee.jw@gmail.com>
2021-06-10 03:32:51 +0200

    Add styles

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,6 +18,7 @@
 #  user_id  (user_id => users.id)
 #
 class Post < ApplicationRecord
+  default_scope { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 150 }
   
