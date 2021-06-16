36565625216a1c691347759dd83bf6c4067857dd
Bakshee <bakshee.jw@gmail.com>
2021-06-16 20:02:15 +0200

    Setup gmail for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
+  host = 'stormy-crag-26703.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.default_options = {from: 'no-reply@herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
