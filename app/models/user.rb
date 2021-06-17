378fb0723fed6d0f7be4589e3cbc0b60e28d4d56
Bakshee <bakshee.jw@gmail.com>
2021-06-17 18:30:31 +0200

    Change feed to followed users' and user's posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -100,7 +100,10 @@ class User < ApplicationRecord
   end
 
   def feed
-    Post.all
+    following_ids = ("SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id")
+    Post.where("user_id IN (#{following_ids}) 
+                OR user_id = :user_id", user_id: id)
   end
 
   private
