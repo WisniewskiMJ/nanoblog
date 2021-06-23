41b440c2a176fae000077535add42524b73a5996
Bakshee <bakshee.jw@gmail.com>
2021-06-23 03:11:51 +0200

    Add seeds file and change db:prepare to db:setup in bin/setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'factory_bot_rails'
+
+User.destroy_all
+
+15.times do
+  FactoryBot.create(:user)
+end
+
+User.all.each { |user| user.activate! }
+
+5.times do
+  User.all.each do |user|
+    rand(0..3).times do
+      FactoryBot.create(:post, user_id: user.id)
+    end
+  end
+end
